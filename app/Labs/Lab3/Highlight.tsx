d357a49486b3e213667150099ff6c23637f67a4e
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-10-19 15:43:47 -0400

    commit 10#

--- app/Labs/Lab3/Highlight.tsx
+++ app/Labs/Lab3/Highlight.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from "react";
 export default function Highlight({ children }: { children: ReactNode }) {
   return (
     <>
-    <h3>Hilight</h3>
-    <span id="wd-highlight" style={{ backgroundColor: "yellow", color: "red" }}>
-      {children}
-    </span>
+      <h3>Highlight</h3>
+      <span id="wd-highlight" style={{ backgroundColor: "yellow", color: "red" }}>
+        {children}
+      </span>
     </>
-    
+
   );
 }
--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -64,7 +64,6 @@ export default function Lab3() {
       <Classes />
       <Styles />
       <Add a={3} b={4} />
-      <hr />
       <h4>Square of 4</h4>
       <Square>4</Square>
       <hr />
