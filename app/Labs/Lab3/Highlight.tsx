a50b522ae526663abed26621b225f9e4c35f4969
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-10-19 15:31:19 -0400

    commit 7#

--- app/Labs/Lab3/Highlight.tsx
+++ app/Labs/Lab3/Highlight.tsx
@@ -1,8 +1,12 @@
 import { ReactNode } from "react";
 export default function Highlight({ children }: { children: ReactNode }) {
   return (
+    <>
+    <h3>Hilight</h3>
     <span id="wd-highlight" style={{ backgroundColor: "yellow", color: "red" }}>
       {children}
     </span>
+    </>
+    
   );
 }
