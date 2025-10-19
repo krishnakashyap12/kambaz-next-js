30fdafcbc2f3ca3253d34e1d8bedcac1ccd272be
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-10-19 15:08:00 -0400

    commit 4#

--- app/Labs/Lab3/FindIndex.tsx
+++ app/Labs/Lab3/FindIndex.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-
 export default function FindIndex() {
-  const numberArray1 = [1, 2, 4, 5, 6];
-  const stringArray1 = ['string1', 'string3'];
-
-  const fourIndex = numberArray1.findIndex(a => a === 4);
-  const string3Index = stringArray1.findIndex(a => a === 'string3');
-
-  return (
-    <div>
-      <h2>FindIndex function</h2>
-      <p>fourIndex = {fourIndex}</p>
-      <p>string3Index = {string3Index}</p>
-    </div>
-  );
-}
+    const numberArray1 = [1, 2, 4, 5, 6];
+    const stringArray1 = ["string1", "string3"];
+  
+    const fourIndex = numberArray1.findIndex(a => a === 4);
+    const string3Index = stringArray1.findIndex(a => a === "string3");
+  
+    return (
+      <div id="wd-find-index">
+        <h4>FindIndex Function</h4>
+        numberArray1 = {numberArray1} <br />
+        stringArray1 = {stringArray1} <br />
+        fourIndex = {fourIndex} <br />
+        string3Index = {string3Index} <hr />
+      </div>
+    );
+  }
